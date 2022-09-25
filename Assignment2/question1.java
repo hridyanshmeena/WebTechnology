c57f76ad76fdcf0b0979148ef9bfbb1ec1bda393
hridyanshmeena <43299684+hridyanshmeena@users.noreply.github.com>
2022-09-25 23:42:35 +0530

    Add files via upload

---
+++ Assignment2/question1.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Asus
+ */
+public class question1 {
+    public static void main(String[] args) {
+
+    double n = 81;
+    if(n>80 && n<90)
+    {
+        n = n + 5;
+    }
+    System.out.println("Result = " + n);
+  }
+    
+}
---
+++ Assignment2/question2.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Asus
+ */
+public class question2 {
+    public static void main(String[] args) {
+    int a = 5;
+    int b = 10;
+    int c = 8;
+ 
+    if(a<b && a<c)
+    {
+        System.out.println("Smallest No. is  " + a);
+    }
+    else if(b<a && b<c)
+    {
+        System.out.println("Smallest No. is  " + b);
+    }
+    else if(c<a && c<b)
+    {
+        System.out.println("Smallest No. is  " + c);
+    }
+  }
+    
+}
